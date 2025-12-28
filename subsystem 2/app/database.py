80b0261bf209649c1973627124de557f35da1f96
Benson Musonda <70434034+Bensonmusonda@users.noreply.github.com>
2025-12-28 17:28:51 +0200

    Clear database URL in database.py
    Updated SQLALCHEMY_DATABASE_URL to an empty string.

--- subsystem 2/app/database.py
+++ subsystem 2/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:nosneb1010?@localhost/subsystem2"
+SQLALCHEMY_DATABASE_URL = ""
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
